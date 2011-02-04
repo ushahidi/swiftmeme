af227e057cc65feb0f6ca0a8ae4a0d1402bc53a4
Ahmed Maawy <ultimateprogramer@yahoo.com>
2011-02-04 15:43:27 +0300

    Adding new functionality to the controllers for the maps - untested

--- web/application/classes/controller/api/activesources.php
+++ web/application/classes/controller/api/activesources.php
@@ -25,6 +25,47 @@ class Controller_Api_ActiveSources extends Controller
         $this->request->response = $activesources;
     }
 
+    public function action_getdayrangegraph($timelimit, $dayto)
+    {
+        $activesources_params = array("RequestType" => "SourcesByChannelOverTime", "Parameters" => array("TimeLimit" => $timelimit, "TimeTo" =>  $dayto));
+        $json_encoded_params = json_encode($activesources_params);
+        $activesources_json = Analytics::analytics_api()->get_analysis($json_encoded_params);
+
+        $activesources = View::factory("parts/activesourcesgraph")->set('json', $activesources_json)->set('timelimit', $timelimit);
+
+        // Render the graph
+        $this->request->response = $activesources;
+    }
+
+    public function action_gettimerangegraph($timefrom, $timeto)
+    {
+        $activesources_params = array("RequestType" => "SourcesByChannelOverTime", "Parameters" => array("TimeRange" => "yes", "TimeFrom" => $timefrom, "TimeTo" =>  $timeto));
+        $json_encoded_params = json_encode($activesources_params);
+        $activesources_json = Analytics::analytics_api()->get_analysis($json_encoded_params);
+
+        $timelimit = 0;
+
+        $time_from = intval(date('z', $timefrom));
+        $time_to = intval(date('z', $timeto));
+
+        if($time_from > $time_to) {
+            if(date('L', $timefrom) == 1) {
+                $timelimit = (366 - $time_from) + $time_to;
+            }
+            else {
+                $timelimit = (365 - $time_from) + $time_to;
+            }
+        }
+        else {
+            $timelimit = $time_from - $time_to;
+        }
+
+        $activesources = View::factory("parts/activesourcesgraph")->set('json', $activesources_json)->set('timelimit', $timelimit);
+
+        // Render the graph
+        $this->request->response = $activesources;
+    }
+
     public function action_test()
     {
         // Get the graph - for the past 7 days
--- web/application/classes/controller/api/trendingkeywords.php
+++ web/application/classes/controller/api/trendingkeywords.php
@@ -22,6 +22,46 @@ class Controller_Api_TrendingKeywords extends Controller
         $this->request->response = $trendingkeywords;
     }
 
+    public function action_getdayrangegraph($timelimit, $dayto)
+    {
+        // Get the graph - for the past 7 days
+        $trendingkeywords_params = array("RequestType" => "ContentByChannelOverTime", "Parameters" => array("TimeLimit" => $timelimit, "TimeTo" => $dayto));
+        $json_encoded_params = json_encode($trendingkeywords_params);
+        $trendingkeywords_json = Analytics::analytics_api()->get_analysis($json_encoded_params);
+
+        $trendingkeywords = View::factory("parts/trendingkeywordsgraph")->set('json', $trendingkeywords_json)->set('timelimit', $timelimit);
+
+        $this->request->response = $trendingkeywords;
+    }
+
+    public function action_gettimerangegraph($timefrom, $timeto)
+    {
+        $trendingkeywords_params = array("RequestType" => "ContentByChannelOverTime", "Parameters" => array("TimeRange" => "yes", "TimeFrom" => $timefrom, "TimeTo" => $timeto));
+        $json_encoded_params = json_encode($trendingkeywords_params);
+        $trendingkeywords_json = Analytics::analytics_api()->get_analysis($json_encoded_params);
+        
+        $time_from = intval(date('z', $timefrom));
+        $time_to = intval(date('z', $timeto));
+
+        $timelimit = 0;
+
+        if($time_from > $time_to) {
+            if(date('L', $timefrom) == 1) {
+                $timelimit = (366 - $time_from) + $time_to;
+            }
+            else {
+                $timelimit = (365 - $time_from) + $time_to;
+            }
+        }
+        else {
+            $timelimit = $time_from - $time_to;
+        }
+
+        $trendingkeywords = View::factory("parts/trendingkeywordsgraph")->set('json', $trendingkeywords_json)->set('timelimit', $timelimit);
+
+        $this->request->response = $trendingkeywords;
+    }
+
     public function action_test()
     {
         // Get the graph - for the past 7 days
