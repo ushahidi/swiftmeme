b8b6655512826ec4c45187359102c8ef8a6257ab
Charl van Niekerk <charlvn@charlvn.com>
2011-05-18 23:34:53 +0200

    Added decorators.

--- swiftmeme.py
+++ swiftmeme.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, session
 
 app = Flask(__name__)
 
@@ -7,19 +7,35 @@ def show(template):
     if "user" in session: vars["user"] = session["user"]
     return render_template(template, **vars)
 
+def loggedin(f):
+    def new():
+        return f() if "user" in session else redirect("/login")
+    return new
+
+def loggedout(f):
+    def new():
+        return redirect("/dashboard") if "user" in session else f()
+    return new
+
 @app.route("/")
+@loggedout
 def index():
     return show("index.html")
 
+@app.route("/dashboard")
+@loggedin
+def dashboard():
+    return show("dashboard.html")
+
 @app.route("/login")
 def login():
-    session["user"] = 1
-    return redirect(url_for("index"))
+    if not "user" in session: session["user"] = 1
+    return redirect("/dashboard")
 
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("index"))
+    if "user" in session: del session["user"]
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
