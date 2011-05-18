93a16a35a54817e14e0d1eb8bd89904124aa2b20
Charl van Niekerk <charlvn@charlvn.com>
2011-05-18 22:33:18 +0200

    Added basic login system (faking the actual lgin)

--- swiftmeme.py
+++ swiftmeme.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, session, redirect, render_template, url_for
 
 app = Flask(__name__)
 
+def show(template):
+    vars = {}
+    if "user" in session: vars["user"] = session["user"]
+    return render_template(template, **vars)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return show("index.html")
+
+@app.route("/login")
+def login():
+    session["user"] = 1
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "abc123"
     app.run(host="0.0.0.0")
