c2268c93a311674ac197a809cda0db3813e01e39
Charl van Niekerk <charlvn@charlvn.com>
2011-05-19 21:02:52 +0200

    Various small improvements.

--- riverid.py
+++ riverid.py
@@ -1,9 +1,25 @@
+# RiverID Python Bindings
+# =======================
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from json import loads
 from urllib import urlencode
 from urllib2 import urlopen
 
 class RiverID(object):
-    def __init__(self, base="http://50.57.68.66/riverid/1/"):
+    def __init__(self, base):
         self.base = base
 
     def authenticate(self, riverid, password):
@@ -18,3 +34,4 @@ class RiverID(object):
         result = loads(urlopen(self.base + "register", urlencode(params)).read())
         if result["status"] == "failure":
             raise Exception(result["response"]["errors"][0])
+        return result["status"] == "success"
