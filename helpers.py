5d4452000a822718b26c88cd1d795da330128310
Charl van Niekerk <charlvn@charlvn.com>
2011-05-23 01:08:09 +0200

    Pass named parameters straight through to render_template in show function.

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 from flask import redirect, render_template, session
 
-def show(template):
-    return render_template(template, session=session)
+def show(template, **vars):
+    return render_template(template, session=session, **vars)
 
 def loggedin(f):
     return lambda: f() if "user" in session else redirect("/login")
