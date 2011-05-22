93444a75836ec9ef678732d7712260a542a6485a
Charl van Niekerk <charlvn@charlvn.com>
2011-05-23 01:12:26 +0200

    Added riverid integration into login code.

--- swiftmeme.py
+++ swiftmeme.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, request, session
 from helpers import loggedin, loggedout, show
+from riverid import RiverID
 
 app = Flask(__name__)
+riverid = RiverID("http://50.57.68.66/riverid/1/")
 
 @loggedout
 @app.route("/")
@@ -16,8 +18,14 @@ def dashboard():
 @app.route("/login")
 def login():
     if request.method == "POST":
-        if not "user" in session: session["user"] = 1
-        return redirect("/dashboard")
+        if "user" in session:
+            return redirect("/dashboard")
+        else:
+            if riverid.authenticate(request.form["riverid"], requst.form["password"]):
+                session["user"] = 1
+                return redirect("/dashboard")
+            else:
+                show("login.html", error=True, riverid=request.form["riverid"])
     else:
         return show("login.html")
 
