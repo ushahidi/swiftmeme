242cf62036dbbdba7e4bf270306fe284ba35267c
Charl van Niekerk <charlvn@charlvn.com>
2011-06-07 15:38:42 +0200

    Added build_request

--- riverid.py
+++ riverid.py
@@ -1,5 +1,5 @@
-# RiverID Bindings
-# ================
+# SwiftRiver Gateway Bindings
+# ===========================
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -17,6 +17,15 @@
 from json    import loads
 from urllib  import urlencode
 from urllib2 import urlopen
+
+def build_request(key, secret, url, method='POST', values={}):
+    params = {"oauth_timestamp": int(time.time()), "oauth_nonce": None, "oauth_signature_method": "HMAC-SHA1"}
+    params.update(values)
+    consumer = oauth2.Consumer(key=key,secret=secret)
+    req = oauth2.Request(method=method, url=url, parameters=params)
+    signature_method = oauth2.SignatureMethod_HMAC_SHA1()
+    req.sign_request(signature_method, consumer, None)
+    return req
 
 class RiverID(object):
     def __init__(self, base):
