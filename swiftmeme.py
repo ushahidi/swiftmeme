f6b6f71c5d2a1f2ef992b2cbdd8fe9ac83d2faa9
Charl van Niekerk <cvn@swiftly.org>
2011-06-15 10:14:41 +0200

    Use built-in jsonify method instead of custom response method.

--- swiftmeme.py
+++ swiftmeme.py
@@ -15,36 +15,31 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from config  import *
-from flask   import Flask, request, response
+from flask   import Flask, jsonify, request
 from gateway import Gateway
 
 app = Flask(__name__)
 gateway = Gateway(GATEWAY_BASE, GATEWAY_KEY, GATEWAY_SECRET)
 
-def response(data):
-    r = make_response(json.dumps(data))
-    r.headers["Content-Type"] = "application/json; charset=UTF-8"
-    return r
-
 @app.route("/api/authenticate", methods=["POST"])
 def login():
-    return response(gateway.authenticate(**request.form))
+    return jsonify(gateway.authenticate(**request.form))
 
 @app.route("/api/register", methods=["POST"])
 def register():
-    return response(gateway.register(**request.form))
+    return jsonify(gateway.register(**request.form))
 
 @app.route("/api/getmemeanalytics", methods=["POST"])
 def getmemeanalytics():
-    return response(gateway.getmemeanalytics(**request.form))
+    return jsonify(gateway.getmemeanalytics(**request.form))
 
 @app.route("/api/getmemecontent", methods=["POST"])
 def getmemecontent():
-    return response(gateway.getmemecontent(**request.form))
+    return jsonify(gateway.getmemecontent(**request.form))
 
 @app.route("/api/getmemeoverview", methods=["POST"])
 def getmemeoverview():
-    return response(gateway.getmemeoverview(**request.form))
+    return jsonify(gateway.getmemeoverview(**request.form))
 
 def main():
     app.debug = DEBUG_MODE
