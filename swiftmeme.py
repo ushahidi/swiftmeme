40f08605b5a250bc671c4a23896c1ad6ffbb9db2
Charl van Niekerk <cvn@swiftly.org>
2011-06-15 11:16:20 +0200

    Only one controller left.

--- swiftmeme.py
+++ swiftmeme.py
@@ -1,5 +1,5 @@
-# SwiftMeme Controllers
-# =====================
+# SwiftMeme Controller
+# ====================
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
