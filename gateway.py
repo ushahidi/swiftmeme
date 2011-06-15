72fc6074fc7ca8fd339fcc1b05c112d0eee8e0c4
Charl van Niekerk <cvn@swiftly.org>
2011-06-15 12:34:27 +0200

    Added memcache support.

--- gateway.py
+++ gateway.py
@@ -14,29 +14,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import oauth2, time, urllib, urllib2
+import hashlib, json, oauth2, time, urllib, urllib2
 
 class Gateway(object):
-    def __init__(self, base, key, secret):
+    def __init__(self, base, key, secret, memcache):
         self.base = base
         self.key = key
         self.secret = secret
+        self.memcache = memcache
 
-    def __request(self, key, secret, path, parameters={}, method="POST"):
-        url = self.base + path
+    def __request(self, key, secret, path, parameters={}, method="POST", cache=False):
+        if cache:
+            key = hashlib.sha512(json.dumps((key, secret, path, parameters, method))).hexdigest()
+            value = self.memcache.get(key)
+        else:
+            value = False
 
-        oauth_parameters = parameters.copy()
-        oauth_parameters["oauth_timestamp"] = int(time.time())
-        oauth_parameters["oauth_nonce"] = None
-        oauth_parameters["oauth_signature_method"] = "HMAC-SHA1"
-        oauth_consumer = oauth2.Consumer(key=key, secret=secret)
-        oauth_request = oauth2.Request(method=method, url=url, parameters=oauth_parameters)
-        oauth_signature_method = oauth2.SignatureMethod_HMAC_SHA1()
-        oauth_request.sign_request(oauth_signature_method, oauth_consumer, None)
+        if not value:
+            url = self.base + path
 
-        http_data = urllib.urlencode(parameters)
-        http_request = urllib2.Request(url, headers=oauth_request.to_header(), data=http_data)
-        http_response = urllib2.urlopen(http_request).read()
+            oauth_parameters = parameters.copy()
+            oauth_parameters["oauth_timestamp"] = int(time.time())
+            oauth_parameters["oauth_nonce"] = None
+            oauth_parameters["oauth_signature_method"] = "HMAC-SHA1"
+            oauth_consumer = oauth2.Consumer(key=key, secret=secret)
+            oauth_request = oauth2.Request(method=method, url=url, parameters=oauth_parameters)
+            oauth_signature_method = oauth2.SignatureMethod_HMAC_SHA1()
+            oauth_request.sign_request(oauth_signature_method, oauth_consumer, None)
+
+            http_data = urllib.urlencode(parameters)
+            http_request = urllib2.Request(url, headers=oauth_request.to_header(), data=http_data)
+            http_response = urllib2.urlopen(http_request)
+
+            value = http_response.read()
+
+            if cache:
+                self.memcache.set(key, value)
 
         return http_response
 
@@ -44,13 +57,13 @@ class Gateway(object):
         return self.__request(self.key, self.secret, "swiftmeme/1/authenticate", {"riverid": riverid, "password": password})
 
     def getmemeanalytics(self, id, secret):
-        return self.__request(id, secret, "swiftmeme/1/getmemeanalytics")
+        return self.__request(id, secret, "swiftmeme/1/getmemeanalytics", cache=True)
 
     def getmemecontent(self, id, secret):
-        return self.__request(id, secret, "swiftmeme/1/getmemecontent")
+        return self.__request(id, secret, "swiftmeme/1/getmemecontent", cache=True)
 
     def getmemeoverview(self, id, secret):
-        return self.__request(id, secret, "swiftmeme/1/getmemeoverview")
+        return self.__request(id, secret, "swiftmeme/1/getmemeoverview", cache=True)
 
     def register(self, riverid, password, emailaddress):
         return self.__request(self.key, self.secret, "swiftmeme/1/register", {"riverid": riverid, "password": password, "emailaddress": emailaddress})
--- swiftmeme.py
+++ swiftmeme.py
@@ -14,10 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import config, flask, gateway
+import config, flask, gateway, memcache
 
 app = flask.Flask(__name__)
 gw = gateway.Gateway(config.GATEWAY_BASE, config.GATEWAY_KEY, config.GATEWAY_SECRET)
+memcache = memcache.Client(config.MEMCACHE_HOSTS)
 
 @app.route("/api/<method>", methods=["POST"])
 def api(method):
