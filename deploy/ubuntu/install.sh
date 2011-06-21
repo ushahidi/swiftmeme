02b73f5baad5213646f41ecd2e70b8201bfb25b3
Charl van Niekerk <charlvn@charlvn.com>
2011-06-21 21:37:48 +0200

    Added deployment script.

---
+++ deploy/ubuntu/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+aptitude update
+aptitude safe-upgrade -y
+aptitude install -y apache2 libapache2-mod-wsgi memcached python-pip git-core
+pip install Flask oauth2 python-memcached
+adduser --disabled-password --gecos "" swiftmeme
+cd /var/www
+git clone https://github.com/ushahidi/SwiftMeme.git swiftmeme
+cp swiftmeme/deploy/ubuntu/000-default /etc/apache2/sites-enabled/
+/etc/init.d/apache2 reload
