0d87c8bdde02b7cbc53089d3d16687dfcbdff813
Charl van Niekerk <cvn@swiftly.org>
2011-06-23 14:12:16 +0200

    Open config in vim.

--- deploy/ubuntu/install.sh
+++ deploy/ubuntu/install.sh
@@ -9,3 +9,4 @@ git clone https://github.com/ushahidi/SwiftMeme.git swiftmeme
 cp swiftmeme/deploy/ubuntu/000-default /etc/apache2/sites-enabled/
 /etc/init.d/apache2 reload
 cp swiftmeme/api/config.example.py swiftmeme/api/config.py
+vim /var/www/swiftmeme/api/config.py
