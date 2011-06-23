a3920357f0b9b8b3d63e442ab2095e733be7c225
Charl van Niekerk <cvn@swiftly.org>
2011-06-23 13:12:34 +0200

    Imported Joe's templates.

---
+++ static/js/myScript.js
@@ -0,0 +1,45 @@
+// scroll back to top
+var $ = jQuery.noConflict(); 
+
+//cufon
+Cufon.replace('h1, h2, h3, h4, h5, h6, label');
+			Cufon.replace('.mainMenu> li > a',{
+						  hover: true						             
+			});
+
+//back to top			
+$(document).ready(function() {
+			
+
+/* top menu */
+$(".mainMenu > li").hover(function() {   
+    $(this).find(".subMenu").animate({ 
+                                 height: "show"
+                                 },150);
+},function(){
+    $(this).find(".subMenu").delay(300).animate({
+                                 height: "hide"
+                                 },150);
+    });
+
+// animate social links
+$('.socialDetails a').hover(
+							  function(){
+			
+		$(this).stop().animate({"margin-top": "-5px"}, 100);
+		
+	},
+	function(){
+		
+		$(this).stop().animate({"margin-top": "0px"}, 100);
+	
+});
+ 
+//jquery tabs	
+$(".pageNumbers > ul").tabs("div.portfolioWrap > ul li",{
+						  effect: 'fade',
+						  fadeSpeed: 300
+							});
+
+
+  }); 
