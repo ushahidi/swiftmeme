a643ef5aa1a567bfeb6e7153d97bf9a7b5b714f8
Charl van Niekerk <charl@ushahidi.com>
2011-06-27 23:29:41 +0200

    Added registration.

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,14 @@
 $("#loginBox").live("submit", function() {
- $.getJSON("/api/authenticate", {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()}, function(data) {
+ var params = {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()};
+ $.getJSON("/api/authenticate", params, function(data) {
+  localStorage.setItem("memes", JSON.stringify(data.response.memes));
+  location = "/dashboard";
+ });
+ return false;
+});
+$("#signupBox").live("submit", function() {
+ var params = {"riverid": $("#registerriverid").val(), "password": $("#registerpassword").val(), "emailaddress": $("#registeremailaddress").val()};
+ $.getJSON("/api/register", params, function(data) {
   localStorage.setItem("memes", JSON.stringify(data.response.memes));
   location = "/dashboard";
  });
