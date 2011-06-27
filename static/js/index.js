bbd6902eb63cc6885f5abbed8bacfaf37c4fb9d1
Charl van Niekerk <charl@ushahidi.com>
2011-06-27 21:29:04 +0200

    Use jQuery live

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-$("#loginBox").submit(function() {
+$("#loginBox").live("submit", function() {
  $.getJSON("/api/authenticate", {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()}, function(data) {
   localStorage.setItem("memes", JSON.dumps(data.response.memes));
   alert(localStorage.getItem("memes"));
