1e2a10317a0d1ef4004713ece9dbf41490db7f3b
Charl van Niekerk <charl@ushahidi.com>
2011-06-27 21:32:48 +0200

    Fixed json dumping

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $("#loginBox").live("submit", function() {
  $.getJSON("/api/authenticate", {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()}, function(data) {
-  localStorage.setItem("memes", JSON.dumps(data.response.memes));
+  localStorage.setItem("memes", JSON.stringify(data.response.memes));
   alert(localStorage.getItem("memes"));
  });
  return false;
