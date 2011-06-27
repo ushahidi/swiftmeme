a77da2cbf42d566ce0ae0be25160732651837713
Charl van Niekerk <charl@ushahidi.com>
2011-06-27 21:13:27 +0200

    Fixed loginbutton selector

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-$("#loginBox button").submit(function() {
+$("#loginbutton").submit(function() {
  $.getJSON("/api/authenticate", {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()}, function(data) {
   localStorage.setItem("memes", JSON.dumps(data.response.memes));
   alert(localStorage.getItem("memes"));
