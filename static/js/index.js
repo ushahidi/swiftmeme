53f32b75a0efbaed9aa78160c7bdc4cd6842855f
Charl van Niekerk <charl@ushahidi.com>
2011-06-27 21:35:35 +0200

    Added dashboard redirect.

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 $("#loginBox").live("submit", function() {
  $.getJSON("/api/authenticate", {"riverid": $("#loginriverid").val(), "password": $("#loginpassword").val()}, function(data) {
   localStorage.setItem("memes", JSON.stringify(data.response.memes));
+  location = "/dashboard";
  });
  return false;
 });
