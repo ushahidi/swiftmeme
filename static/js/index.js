f3a0a31c3b71b5e39905a42cd1f0f039dfc562b7
Charl van Niekerk <charl@ushahidi.com>
2011-06-29 15:48:53 +0200

    Fixed double affero

--- static/js/index.js
+++ static/js/index.js
@@ -12,7 +12,7 @@
  * SwiftMeme is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero Affero General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with SwiftMeme.  If not, see <http://www.gnu.org/licenses/>.
