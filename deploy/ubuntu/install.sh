be2ffee866104a3d13b22dc5c67898dd54f28be2
Charl van Niekerk <charl@ushahidi.com>
2011-06-29 15:48:25 +0200

    Added licensing and comments.

--- deploy/ubuntu/install.sh
+++ deploy/ubuntu/install.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
+#
+# SwiftRiver Ubuntu Deployment Bash Script
+# ========================================
+#
+# This file is part of SwiftMeme.
+#
+# SwiftMeme is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SwiftMeme is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with SwiftMeme.  If not, see <http://www.gnu.org/licenses/>.
+
+# Apply all system updates.
 aptitude update
 aptitude safe-upgrade -y
+
+# Install the necessary Ubuntu packages.
 aptitude install -y apache2 libapache2-mod-wsgi memcached python-pip git-core
+
+# Install the necessary Python packages.
 pip install Flask oauth2 python-memcached
+
+# Create a user for SwiftMeme processes to run as.
 adduser --disabled-password --gecos "" swiftmeme
+
+# Create a local clone of the application.
 cd /var/www
 git clone https://github.com/ushahidi/SwiftMeme.git swiftmeme
+
+# Replace the default Apache configuration with the bundled one.
 cp swiftmeme/deploy/ubuntu/000-default /etc/apache2/sites-enabled/
+
+# Tell Apache to reload its configuration.
 /etc/init.d/apache2 reload
+
+# Copy the example SwiftMeme configuration file for customisation.
 cp swiftmeme/api/config.example.py swiftmeme/api/config.py
+
+# Open the configuration file in vim.
 vim /var/www/swiftmeme/api/config.py
