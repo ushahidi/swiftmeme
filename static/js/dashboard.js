621c9a48396cb056a10c855437864dc928933654
Charl van Niekerk <charl@ushahidi.com>
2011-07-04 14:48:39 +0200

    Added getmemeanalytics call

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,3 +1,10 @@
 (function() {
-alert(localStorage.getItem("memes"));
+ var memes = localStorage.getItem("memes");
+ for (var i = 0; i < memes.length; i++) {
+  $.getJSON("/api/getmemeanalytics", {id: memes[i].id, secret: memes[i].secret}, function(data) {
+   if (data.status == "success") {
+    alert(data);
+   }
+  });
+ }
 })();
